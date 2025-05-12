65cfb63328ee80aa206a1556f80481bb4233a92a
HenryOfc <cuentass908@gmail.com>
2025-05-12 11:42:42 -0500

    Update dl-play.js

--- plugins/dl-play.js
+++ plugins/dl-play.js
@@ -4,7 +4,7 @@ let limit = 320;
 let confirmation = {};
 
 let handler = async (m, { conn, command, text, args, usedPrefix }) => {
-    if (!text) throw `✳️ ${mssg.example} *${usedPrefix + command}* Lil Peep hate my life`;
+    if (!text) throw `✳️ Ejemplo *${usedPrefix + command}* Lil Peep hate my life`;
 
     let res = await yts(text);
     let vid = res.videos[0];
@@ -21,42 +21,39 @@ let handler = async (m, { conn, command, text, args, usedPrefix }) => {
     let playMessage = `
 ≡ *HJ MUSIC*
 ┌──────────────
-▢ 📌 *${mssg.title}:* ${vid.title}
-▢ 📆 *${mssg.aploud}:* ${vid.ago}
-▢ ⌚ *${mssg.duration}:* ${vid.timestamp}
-▢ 👀 *${mssg.views}:* ${vid.views.toLocaleString()}
+☆ 📌 *Título:* ${vid.title}
+☆ 📆 *Fecha:* ${vid.ago}
+☆ ⌚ *Duración:* ${vid.timestamp}
+☆ 👀 *Vistas:* ${vid.views.toLocaleString()}
 └──────────────`;
 
-if(business){
-    conn.sendFile(m.chat, thumbnail, "error.jpg", `${playMessage}\n\nEscribe:\n1️⃣ para recibir el archivo como MP3.\n2️⃣ para recibir el archivo como MP4.`, m);
+    if (business) {
+        conn.sendFile(m.chat, thumbnail, "error.jpg", `${playMessage}\n\nEscribe:\n1️⃣ para recibir el archivo como MP3.\n2️⃣ para recibir el archivo como MP4.`, m);
 
-    // 
-    confirmation[m.sender] = {
-        sender: m.sender,
-        to: who,
-        url: url,
-        chat: chat, 
-        timeout: setTimeout(() => {
-            delete confirmation[m.sender];
-
-            //conn.reply(m.chat, `⏳ Tiempo de respuesta agotado. Vuelve a intentarlo.`, m);
-        }, 60000), // 1 minuto de espera
-    };
-} else {
-    conn.sendButton(m.chat, playMessage, mssg.ig, thumbnail, [
-        ['🎶 MP3', `${usedPrefix}fgmp3 ${url}`],
-        ['🎥 MP4', `${usedPrefix}fgmp4 ${url}`]
-      ], m)
+        confirmation[m.sender] = {
+            sender: m.sender,
+            to: who,
+            url: url,
+            chat: chat, 
+            timeout: setTimeout(() => {
+                delete confirmation[m.sender];
+            }, 60000), // 1 minuto de espera
+        };
+    } else {
+        conn.sendButton(m.chat, playMessage, thumbnail, [
+            ['🎶 MP3', `${usedPrefix}fgmp3 ${url}`],
+            ['🎥 MP4', `${usedPrefix}fgmp4 ${url}`]
+        ], m);
+    }
 }
 
-
-}
 handler.help = ['play'];
 handler.tags = ['dl'];
-handler.command = ['play','playvid'];
+handler.command = ['play', 'playvid'];
 handler.disabled = false;
 
 export default handler;
+
 handler.before = async m => {
     if (m.isBaileys) return; // Ignorar mensajes del bot
     if (!(m.sender in confirmation)) return; // Solo continuar si hay confirmación pendiente
@@ -66,26 +63,25 @@ handler.before = async m => {
         clearTimeout(timeout);
         delete confirmation[m.sender];
 
-        let res = await fetch(global.API('fgmods', '/api/downloader/ytmp3', { url: url }, 'apikey'));
+        let res = await fetch(`https://api.fgmods.xyz/api/downloader/ytmp3?url=${url}&apikey=fg_M6khGFXR`);
         let data = await res.json();
 
         let { title, dl_url, thumb, size, sizeB, duration } = data.result;
-        conn.sendFile(m.chat, dl_url, title + '.mp3', `≡  *HJ YTDL*\n\n▢ *📌 ${mssg.title}* : ${title}`, m, false, { mimetype: 'audio/mpeg', asDocument: chat.useDocument });
+        conn.sendFile(m.chat, dl_url, title + '.mp3', `≡  *HJ YTDL*\n\n▢ *📌 Título* : ${title}`, m, false, { mimetype: 'audio/mpeg', asDocument: chat.useDocument });
         m.react('✅');
     } else if (m.text.trim() === '2') {
         clearTimeout(timeout);
         delete confirmation[m.sender];
 
-        let res = await fetch(global.API('fgmods', '/api/downloader/ytmp4', { url: url }, 'apikey'));
+        let res = await fetch(`https://api.fgmods.xyz/api/downloader/ytmp4?url=${url}&apikey=fg_M6khGFXR`);
         let data = await res.json();
 
         let { title, dl_url, thumb, size, sizeB, duration } = data.result;
         let isLimit = limit * 1024 < sizeB;
 
-        await conn.loadingMsg(m.chat, '📥 Descargando', ` ${isLimit ? `≡  *HJ YTDL*\n\n▢ *⚖️${mssg.size}*: ${size}\n\n▢ _${mssg.limitdl}_ *+${limit} MB*` : '✅ Descarga Completada' }`, ["▬▭▭▭▭▭", "▬▬▭▭▭▭", "▬▬▬▭▭▭", "▬▬▬▬▭▭", "▬▬▬▬▬▭", "▬▬▬▬▬▬"], m);
+        await conn.loadingMsg(m.chat, '📥 Descargando', ` ${isLimit ? `≡  *HJ YTDL*\n\n▢ *⚖️ Tamaño*: ${size}\n\n▢ _Limite_ *+${limit} MB*` : '✅ Descarga Completada' }`, ["▬▭▭▭▭▭", "▬▬▭▭▭▭", "▬▬▬▭▭▭", "▬▬▬▬▭▭", "▬▬▬▬▬▭", "▬▬▬▬▬▬"], m);
 
-        if (!isLimit) conn.sendFile(m.chat, dl_url, title + '.mp4', `≡  *HJ YTDL*\n*📌${mssg.title}:* ${title}\n*⚖️${mssg.size}:* ${size}`, m, false, { asDocument: chat.useDocument });
+        if (!isLimit) conn.sendFile(m.chat, dl_url, title + '.mp4', `≡  *HJ YTDL*\n*📌 Título:* ${title}\n*⚖️ Peso* ${size}`, m, false, { asDocument: chat.useDocument });
         m.react('✅');
     }
-
 }
