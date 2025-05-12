825e28b91e2595b913a63c7b06f55f762de15f7f
HenryOfc <cuentass908@gmail.com>
2025-05-12 11:06:08 -0500

    Update nsfw-xnxxdl.js

--- plugins/nsfw-xnxxdl.js
+++ plugins/nsfw-xnxxdl.js
@@ -38,8 +38,6 @@ let handler = async (m, { conn, args, text, usedPrefix, command }) => {
 handler.help = ['xnxx'] 
 handler.tags = ['nsfw', 'prem']
 handler.command = ['xnxxsearch', 'xnxxdl', 'xnxx'] 
-handler.diamond = 2
-handler.premium = false
 handler.register = true
 
 export default handler
